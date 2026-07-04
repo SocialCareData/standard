ee82b6119b73338f07862dbb0b5706a28c640b69
Matthieu Bosquet <matthieubosquet@gmail.com>
2026-07-04 12:00:19 +0100

    Check internal links are relative

--- src/_plugins/check_internal_links.rb
+++ src/_plugins/check_internal_links.rb
@@ -5,8 +5,11 @@ Jekyll::Hooks.register :site, :post_write do |site|
   require 'nokogiri'
   require 'uri'
 
-  dest   = site.dest
-  broken = []
+  dest       = site.dest
+  broken     = []
+  absolute   = []
+  site_url   = site.config['url'].to_s.strip
+  site_host  = site_url.empty? ? nil : URI(site_url).host
 
   Find.find(dest) do |path|
     next unless path.end_with?('.html')
@@ -16,6 +19,14 @@ Jekyll::Hooks.register :site, :post_write do |site|
       href = a['href'].to_s.strip
       next if href.empty?
       next if href.start_with?('#', 'mailto:', 'tel:')
+
+      # Flag internal links written as absolute URLs to the site's own host;
+      # these should be relative instead.
+      if site_host && href.start_with?('http://', 'https://') && URI(href).host == site_host
+        absolute << "#{path}: #{href}"
+        next
+      end
+
       next unless href.start_with?('/')
 
       target     = URI(href).path
@@ -35,6 +46,12 @@ Jekyll::Hooks.register :site, :post_write do |site|
   else
     Jekyll.logger.error('Internal links:', "#{broken.size} broken link(s):")
     broken.each { |b| Jekyll.logger.error('', "  #{b}") }
-    exit 1
   end
+
+  unless absolute.empty?
+    Jekyll.logger.error('Internal links:', "#{absolute.size} absolute internal link(s) (should be relative):")
+    absolute.each { |b| Jekyll.logger.error('', "  #{b}") }
+  end
+
+  exit 1 unless broken.empty? && absolute.empty?
 end
